b8fc8d7df3bef55a24672259c3bbde4a2ea4c922
sawyersieja <sawyersieja@protonmail.com>
2024-06-09 15:09:59 -0700

    Saving local progress. Irrelevant changes.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     
 });
+
+const heroSection = document.getElementById("hero-section");
+const aboutSection = document.getElementById("about-section");
+const skillsSection = document.getElementById("skills-section");
+const portfolioSection = document.getElementById("portfolio-section");
+const contactSection = document.getElementById("contact-section");
+
+let currentSection = heroSection;
+let previousSection = null;
+let nextSection = aboutSection;
+
+//what are you trying to do?
+//I want to auto scroll up and down
+
+document.onscroll = function scroll() {
+
+    //aboutSection.scrollIntoView({behavior: "smooth"});
+}
